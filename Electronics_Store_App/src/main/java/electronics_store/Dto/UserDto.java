eb21dbf32aaa943ccb1b74caca1c418ffab13418
Manojmali100 <manojmali29684@gmail.com>
2025-03-29 09:42:52 +0530

    Validetion apply in project

--- Electronics_Store_App/src/main/java/electronics_store/Dto/UserDto.java
+++ Electronics_Store_App/src/main/java/electronics_store/Dto/UserDto.java
@@ -1,12 +1,21 @@
 package electronics_store.Dto;
 
+<<<<<<< HEAD
 import lombok.*;
+=======
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+>>>>>>> a536eb9 ( Validetion apply in project)
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class UserDto {
+<<<<<<< HEAD
     private String UserId;
 
     private String name;
@@ -20,4 +29,19 @@ public class UserDto {
     private String about;
 
     private String imageName;
+=======
+	private String UserId;
+	@Size(min = 3, max = 15, message = "Invalid name !!")
+	private String name;
+	@Email(message = "Invalid User Email !!")
+	private String email;
+	@NotBlank(message = "Password is required !!")
+	private String password;
+	@Size(min = 3, max = 6, message = "Invalid Gender !!")
+	private String gender;
+	@NotBlank(message="Write Somthing YourSelf !!")
+	private String about;
+
+	private String imageName;
+>>>>>>> a536eb9 ( Validetion apply in project)
 }
